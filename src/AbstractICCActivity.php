f1c791f71b866d0f5c3d1b59198d6f8bbc5ef84f
lxz <lxz2133@gmail.com>
2018-09-06 14:37:30 +0800

    提交icc-activity

---
+++ src/AbstractICCActivity.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lxz
+ * Date: 2018/9/6
+ * Time: 14:24
+ */
+
+namespace ICCGame\Activity;
+
+abstract class AbstractICCActivity implements ICCActivityInterface
+{
+    public function checkActivityTime()
+    {
+        // TODO: Implement checkActivityTime() method.
+    }
+}
---
+++ src/ICCActivityInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lxz
+ * Date: 2018/9/6
+ * Time: 14:04
+ */
+
+namespace ICCGame\Activity;
+
+interface ICCActivityInterface
+{
+
+    public function checkActivityTime();
+
+}
